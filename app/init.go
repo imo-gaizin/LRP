8ca3c13bf8546ac04e772fa77b9d79d874c8da6d
Yuukichi Nankou <yuukichi_nankou@x-trans.jp>
2017-08-01 20:19:18 +0900

    test

--- app/init.go
+++ app/init.go
@@ -7,17 +7,18 @@ import (
 
 func init() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprint(w,
-          []byte(`
-            <html>
-              <head>
-                <title>チャット</title>
-              </head>
-              <body>
-                チャットしましょう!
-              </body>
-            </html>
-          `)
+        fmt.Fprint(w, "へろーわーるどやで！！！")
+        w.Write(
+            []byte(`
+              <html>
+                <head>
+                  <title>チャット</title>
+                </head>
+                <body>
+                  チャットしましょう!
+                </body>
+              </html>
+            `)
         )
     })
 }
